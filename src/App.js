4b61c58ca270434d795b584f9f781d693a3d6cdd
vssaugusto@gmail.com <33253737+ViniciusAugusto20@users.noreply.github.com>
2020-04-12 19:01:47 -0300

    Commit criação de uma listagem
    Foi realizada a criação de uma lista simples com a função de adicionar um projeto com valor referente a hora atual e remover projetos listados.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositorys, setRepositorys] = useState([]);
+
+  useEffect(() => {
+    //Não pode usar por enquanto
+    api.get("/repositories").then((response) => {
+      setRepositorys(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("/repositories", {
+      title: `Novo projeto ${Date.now()}`,
+      url: "https://www.google.com.br",
+      techs: ["NodeJs", "ReactJS"],
+    });
+
+    setRepositorys([...repositorys, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+    setRepositorys(repositorys.filter((repository) => repository.id !== id));
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    <>
+      <div>
+        <ul data-testid="repository-list">
+          {repositorys.map((repository) => (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+            </li>
+          ))}
+        </ul>
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+        <p />
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
+    </>
   );
 }
 
